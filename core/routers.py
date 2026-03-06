d5b8723a5488b7e58dcebe5b19a885f42ad370ce
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-03-06 10:04:51 +0300

    Bump psycopg2-binary to 2.9.9 and add refresh token support

--- core/routers.py
+++ core/routers.py
@@ -1,5 +1,6 @@
 from rest_framework import routers
-from core.auth.viewsets.register import RegisterViewSet
+from core.auth.viewsets import RegisterViewSet,LoginViewSet,RefreshTokenViewSet
+# from core.auth.viewsets.login import LoginViewSet
 
 
 
@@ -9,11 +10,14 @@ from core.user.viewsets import UserViewset
 
 router=routers.SimpleRouter()
 #####USER###
-
+router.register(r'auth/register',RegisterViewSet,basename='auth-register')
+router.register(r'auth/login',LoginViewSet,basename='auth-login')
 router.register(r'user',UserViewset,basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='register')
+router.register(r'auth/refresh', RefreshTokenViewSet, basename='auth-refresh')
 
 
 urlspatterns= [ *router.urls,]
+
 
 ##AUTH##
