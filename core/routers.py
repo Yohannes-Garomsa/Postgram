54f66db97950a48de74f0ef96575c29f69f32666
Yohannes-Garomsa <yohannesgaromsa051@gmail.com>
2026-03-06 18:04:32 +0300

    post viewsets implemented to  ensure that our should follow :Only authenticated users can create posts,Only authenticated users can read post and only GET and POST method are allowed

--- core/routers.py
+++ core/routers.py
@@ -2,11 +2,8 @@ from rest_framework import routers
 from core.auth.viewsets import RegisterViewSet,LoginViewSet,RefreshTokenViewSet
 # from core.auth.viewsets.login import LoginViewSet
 
-
-
-
-
 from core.user.viewsets import UserViewset
+from core.post.viewsets import PostViewSet
 
 router=routers.SimpleRouter()
 #####USER###
@@ -15,6 +12,7 @@ router.register(r'auth/login',LoginViewSet,basename='auth-login')
 router.register(r'user',UserViewset,basename='user')
 router.register(r'auth/register', RegisterViewSet, basename='register')
 router.register(r'auth/refresh', RefreshTokenViewSet, basename='auth-refresh')
+router.register(r'post', PostViewSet, basename='post')
 
 
 urlspatterns= [ *router.urls,]
